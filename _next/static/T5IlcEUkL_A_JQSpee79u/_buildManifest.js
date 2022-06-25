9d2d84d12e37f2e335937a2aa62490d738454594
SlugboieExtra <98238319+SlugboieExtra@users.noreply.github.com>
2022-06-24 22:45:37 -0300

    V4

---
+++ _next/static/T5IlcEUkL_A_JQSpee79u/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/365-fe7c25472b182a2c.js","static/css/2cb929395e5eff72.css","static/chunks/pages/index-8541502a6f98efb9.js"],"/_error":["static/chunks/pages/_error-a01502ede1684707.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
